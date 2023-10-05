c0c3967b4fb27c2fa97dc836ee75f5c4b7ee105d
Syuji-gooner8 <135284715+Syuji-gooner8@users.noreply.github.com>
2023-10-05 04:42:49 +0000

    bookers2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,15 +3,14 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.save
-    render 'favorite.js.erb'
-    #redirect_to book_path(params[:book_id])
+
+    
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.destroy
-    render 'favorite.js.erb'
-    #redirect_to book_path(params[:book_id])
+
   end
 end
